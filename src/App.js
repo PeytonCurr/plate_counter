90080ea2a7442cb8a769cab5caf29bb8ee8f9d09
PeytonCurr <peyton.curr@gmail.com>
2024-01-21 21:40:06 -0700

    Setting up main layout of app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
 import React from 'react'
 
 function App() {
 
   return (
     <>
-      <section className='flex flex-row'>
-        <div className='text-2xl p-2 justify-center'>Is this still working?</div>
+      <section className='grid grid-rows-3 h-full'>
+        <div className='text-2xl p-2 justify-center bg-red-500'>Is this still working?</div>
       </section>
     </>
   );

d359732a0cebddbe60caec08f8f860c5cbc9da14
PeytonCurr <peyton.curr@gmail.com>
2024-01-21 22:48:43 -0700

    Connecting header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import React from 'react'
+import Navbar from './components/navbar.jsx'
 
 function App() {
 
   return (
     <>
-      <section className='grid grid-rows-3 h-full'>
+      <Navbar></Navbar>
+      <section className='grid grid-col-1 h-screen'>
         <div className='text-2xl p-2 justify-center bg-red-500'>Is this still working?</div>
+        <div className='bg-green-400'>Chicken</div>
+        <div></div>
       </section>
     </>
   );
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='h-20'> Butt </div>
+  );
+}
+
+export default Navbar;

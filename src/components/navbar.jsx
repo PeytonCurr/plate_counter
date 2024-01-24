711cb9a1415a186cb30f416985a3e30bb900bbf0
PeytonCurr <peyton.curr@gmail.com>
2024-01-24 14:29:17 -0700

    Making Logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,14 +4,14 @@ import { mdiDumbbell } from '@mdi/js';
 
 function Navbar() {
   return (
-    <section className='grid grid-col-10'>
-      <div className=''> Account <Icon path={mdiDumbbell} size={1} /></div>
-      <div className="bg-yellow-500">
-        <h1 className="text-2xl p-2">THREE</h1>
+    <section className='grid grid-cols-1'>
+
+      <div className='text-2xl flex items-center p-2'>
+        <Icon path={mdiDumbbell} size={1} color="red" rotate={45} />
+        <span className='border-2 border-l-0 border-red-500 p-1 pr-1.5 pl-0'>PlateCounter</span>
+
       </div>
-      <div className="bg-lime-600">
-        <h1 className="text-2xl p-2 text-red-700">FOUR</h1>
-      </div>
+
     </section>
   )
 }

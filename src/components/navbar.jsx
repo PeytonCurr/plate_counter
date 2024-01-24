4e3858b4ab02d15d882d54ee9b0c50ff604f9138
PeytonCurr <peyton.curr@gmail.com>
2024-01-23 22:20:47 -0700

    Implementing Mdi Icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Icon from '@mdi/react';
+import { mdiAccount } from '@mdi/js';
 
 function Navbar() {
   return (
-    <div className='h-20'> Butt </div>
-  );
+    <section className='grid grid-cols-10'>
+      <div><Icon path={mdiAccount} size={1} /> Account</div>
+    </section>
+  )
 }
 
 export default Navbar;

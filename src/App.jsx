2e2ad4570326c22e3071bbfb5fb40f6fa0f68e77
PeytonCurr <peyton.curr@gmail.com>
2024-01-24 17:31:45 -0700

    Saving for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import logo from './logo.svg';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/navbar.jsx'
 
 function App() {
 
+  const [weight, setWeight] = useState(0);
+
+  function enterWeight() {
+
+  }
+
   return (
     <>
-      <Navbar></Navbar>
-      <section className='grid grid-col-1 h-screen bg-amber-50'>
+      <div>
+        <Navbar></Navbar>
+        <section className='grid grid-col-1 h-screen bg-amber-50'>
 
-        <div className='text-2xl p-2 justify-center'>Dumbbell + Plates Competent</div>
+          <div className='text-2xl p-2 justify-center'>Dumbbell + Plates Competent</div>
 
-        <div className=''>Future Stats Component</div>
+          <div className=''>Future Stats Component</div>
 
-        <div className=''>Enter Weight Component</div>
+          <div className='flex justify-center items-center'>
+            <form onSubmit={enterWeight} className='w-1/3'>
+              <div> Check me out </div>
+              <input type="number" className="w-2/3" />
+              <button type='submit'> Enter </button>
+            </form>
+          </div>
 
-      </section>
+        </section>
+      </div>
     </>
   );
 }

896c9ce9b3740e2498cfaafe1cba694b242012db
PeytonCurr <peyton.curr@gmail.com>
2024-01-26 12:14:27 -0700

    Adding more css changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,12 @@ function App() {
         <Navbar></Navbar>
         <section className='grid grid-col-1 grid-rows-5 p-2 gap-2 bg-blue-100' style={{ height: "92vh" }}>
 
-          <div className='text-2xl p-2 justify-center bg-blue-300 row-span-2 rounded'>Dumbbell + Plates Competent</div>
+          <div className='text-2xl p-2 justify-center bg-blue-300 row-span-2 rounded shadow'>Dumbbell + Plates Competent</div>
 
-          <div className='bg-blue-300 rounded'>Future Stats Component</div>
+          <div className='bg-blue-300 rounded shadow'>Future Stats Component</div>
 
-          <div className='flex justify-center items-center rounded bg-blue-300'>
-            <form onSubmit={handleSubmit} className=' bg-white rounded shadow-sm p-6 pt-4 w-2/3'>
+          <div className='flex justify-center items-center rounded bg-blue-300 shadow'>
+            <form onSubmit={handleSubmit} className=' bg-blue-50 rounded shadow p-6 pt-4 w-2/3'>
               <div className='text-center mb-2'> Enter Weight: {weight} </div>
               <div className='flex justify-center'>
                 <input value={weight || ""} type="number" step={5} max={1000} onChange={handleChange} className="text-center w-3/4 rounded p-1 px-2 border-2" />

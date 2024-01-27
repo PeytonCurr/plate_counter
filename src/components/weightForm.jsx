3b5d55938356d932d2db274de45eef46f936e17f
PeytonCurr <peyton.curr@gmail.com>
2024-01-26 22:42:47 -0700

    Setting up checkmark area

--- src/components/weightForm.jsx
+++ src/components/weightForm.jsx
@@ -14,12 +14,17 @@ function WeightForm(props) {
   return (
     <div className='flex justify-center items-center rounded bg-blue-300 shadow'>
       <form onSubmit={handleSubmit} className=' bg-blue-50 rounded shadow p-6 pt-4 w-2/3'>
-        <div className='text-center mb-2'> Enter Weight: {props.weight} </div>
-        <div className='flex justify-center'>
-          <input type="checkbox" checked={props.barBell} onChange={e => {
-            props.setBarBell(e.target.checked);
-          }} />
+        <h1 className='text-start mb-2'> Enter Weight {props.weight} </h1>
+
+        <div className="flex justify-between">
           <input value={props?.weight || ""} type="number" step={5} max={1000} onChange={handleChange} className="text-center w-3/4 rounded p-1 px-2 border-2" />
+
+          <div>
+            <h1>BarBell?</h1>
+            <input className="" type="checkbox" checked={props.barBell} onChange={e =>
+              props.setBarBell(e.target.checked)} />
+          </div>
+
         </div>
       </form>
     </div>

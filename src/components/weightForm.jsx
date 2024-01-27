61151e9dd7607b0a45cb3f015d4d9b36346f22f0
PeytonCurr <peyton.curr@gmail.com>
2024-01-27 12:19:59 -0700

    Have the WeightForm working and visually showing current states

--- src/components/weightForm.jsx
+++ src/components/weightForm.jsx
@@ -14,16 +14,19 @@ function WeightForm(props) {
   return (
     <div className='flex justify-center items-center rounded bg-blue-300 shadow'>
       <form onSubmit={handleSubmit} className=' bg-blue-50 rounded shadow p-6 pt-4 w-2/3'>
-        <h1 className='text-start mb-2'> Enter Weight {props.weight} </h1>
+        <h1 className='text-start mb-2 flex justify-between'>
+          <span>Enter Weight [ {props.weight} + {props.barBell ?
+            "True" : "False"
+          } ] </span>
+          <span>Barbell?</span>
+        </h1>
 
         <div className="flex justify-between">
-          <input value={props?.weight || ""} type="number" step={5} max={1000} onChange={handleChange} className="text-center w-3/4 rounded p-1 px-2 border-2" />
+          <input value={props?.weight || ""} type="number" step={5} max={1000} onChange={handleChange} className="text-center w-3/4 sm:w-4/5 rounded p-1 px-2 border-2" />
 
-          <div>
-            <h1>BarBell?</h1>
-            <input className="" type="checkbox" checked={props.barBell} onChange={e =>
-              props.setBarBell(e.target.checked)} />
-          </div>
+          <input className="mr-5" type="checkbox" checked={props.barBell} onChange={e =>
+            props.setBarBell(e.target.checked)} />
+
 
         </div>
       </form>

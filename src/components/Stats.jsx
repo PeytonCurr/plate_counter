ee4d29f911e6b81fcb570a89a70bc441328ac536
PeytonCurr <peyton.curr@gmail.com>
2024-01-28 18:39:01 -0700

    Switching to new state data type

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,36 +2,21 @@ import React, { useEffect } from 'react'
 
 function Stats(props) {
 
+  const plates = [45, 35, 25, 10, 5, 2.5];
 
   useEffect(() => {
     let weight = props.weight / 2
-    let plates = [45, 35, 25, 10, 5, "2_5"]
-    let plateCounter = {}
 
-    props.setC45(0)
-    props.setC35(0)
-    props.setC25(0)
-    props.setC10(0)
-    props.setC5(0)
-    props.setC2_5(0)
+    props.resetCounts()
 
     plates.forEach((plate) => {
-      const interFunction = `props.setC${plate}`
-      if (plate == "2_5") {
-        plate = 2.5
-      }
-      plateCounter[plate] = 0
       while (weight >= plate) {
-        plateCounter[plate] += 1
+        props.setCounts({ ...props.counts, [plate]: props.counts[plate] + 1 })
         weight -= plate
-      }
-      if (plateCounter[plate] > 0) {
-        eval(interFunction + `(${plateCounter[plate]})`)
       }
     })
   }, [props.weight]);
 
-  const plates = [45, 35, 25, 10, 5, 2.5];
 
   return (
     <div className='bg-blue-300 rounded shadow grid grid-cols-2 p-2 px-4 gap-4 row-span-1 md:row-span-2'>

82f24a10a7a879ef6e444e8dfb8cdc6c1714e4a2
PeytonCurr <peyton.curr@gmail.com>
2024-02-01 15:41:14 -0700

    Got Some Svgs Displaying on screen

--- src/components/BarbellPlates.jsx
+++ src/components/BarbellPlates.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import BarBellSVG from './svgs/BarBellSVG.jsx'
+import BasePlate45_35SVG from './svgs/BasePlate45_35SVG.jsx'
 
 function BarbellPlates(props) {
   return (
-    <div className='text-2xl p-2 justify-center bg-blue-300 row-span-2 md:row-span-3 rounded shadow'>
-      Dumbbell + Plates Competent
+    <div className='text-2xl p-2 bg-blue-300 row-span-2 md:row-span-3 rounded shadow flex justify-center items-center'>
+      <BasePlate45_35SVG height={200} />
+      <BarBellSVG width={450} />
     </div>
   )
 }
--- src/components/svgs/BarBellSVG.jsx
+++ src/components/svgs/BarBellSVG.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function BarBellSVG() {
+function BarBellSVG(props) {
   return (
-    <svg width="646" height="20" viewBox="0 0 646 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width={props.width} height="20" viewBox="0 0 646 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M644 2H2V18H644V2Z" fill="#808080" stroke="black" stroke-width="4" />
       <path d="M134 2H127V18H134V2Z" fill="black" stroke="black" stroke-width="4" />
       <path d="M161 2H154V18H161V2Z" fill="black" stroke="black" stroke-width="4" />

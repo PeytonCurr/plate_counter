a47ce95499396fc8c6aca1e94a36a9708c7272f6
PeytonCurr <peyton.curr@gmail.com>
2024-02-02 23:33:15 -0700

    Some Conditional rendering of entire components based off screen size

--- src/components/BarbellPlates.jsx
+++ src/components/BarbellPlates.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BarBell from './svgs/BarBell.jsx'
 import BasePlate45_35 from './svgs/BasePlate45_35.jsx'
 
 function BarbellPlates(props) {
+
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth))
+  }, []);
+
   return (
     <div className='text-2xl p-2 bg-blue-300 row-span-2 md:row-span-3 rounded shadow flex justify-center items-center'>
 
       <BasePlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} height={150} transform={"translate(28 0)"} />
-      <BarBell width={300} />
+
+      {width > 400 &&
+        <BarBell width={300} />
+      }
+
       <BasePlate45_35 color={"#DC2626"} transform={"scale(-1,1) translate(28 0)"} height={150} />
     </div>
   )

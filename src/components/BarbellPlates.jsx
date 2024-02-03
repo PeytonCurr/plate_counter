2aeb50bd297c023fa6613063614894acafdbabc1
PeytonCurr <peyton.curr@gmail.com>
2024-02-03 14:25:47 -0700

    Have the SVGs moving with Tailwind

--- src/components/BarbellPlates.jsx
+++ src/components/BarbellPlates.jsx
@@ -8,18 +8,9 @@ import SmartSVG from './SmartSVG.jsx'
 
 function BarbellPlates(props) {
   const plates = [45, 35, 25, 10, 5, 2.5];
-  const [width, setWidth] = useState(window.innerWidth)
-
-  useEffect(() => {
-    window.addEventListener("resize", () => setWidth(window.innerWidth))
-  }, []);
 
   return (
     <div className='text-2xl p-2 bg-blue-300 row-span-2 md:row-span-3 rounded shadow flex justify-center items-center'>
-
-      <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} height={150} transform={`translate(${12 * props.counts[45]} 0)`} />
-
-      <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} height={150} transform={`translate(${12 * props.counts[45]} 0)`} />
 
       <div className='w-1/12'>
         <BasePlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={"translate(0 0)"} />
@@ -34,12 +25,22 @@ function BarbellPlates(props) {
       ))}
 
 
-      <div className='w-[300px] md:w-[300px]'><BarBell /></div>
+      <div className='w-1/2'><BarBell /></div>
 
-      <SmartSVG name={BasePlate45_35} color={" " + (props.counts[45] > 0 && '#DC2626')} transform={"translate(0 0)"} />
+      <div className='w-1/12 relative'>
 
-      <div className='w-1/6'>
-        <BasePlate45_35 color={"#DC2626"} transform={"scale(-1,1) translate(0 0)"} />
+        <SmartSVG name={BasePlate45_35} color={" " + (props.counts[45] > 0 && '#DC2626')} transform={"scale(-1,1) translate(0 0)"} />
+
+        <div className='w-full absolute left-[20px] bottom-0'>
+          <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={`scale(-1,1)`} />
+        </div>
+        <div className='w-full absolute left-[40px] bottom-0'>
+          <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={`scale(-1,1)`} />
+        </div>
+        <div className='w-full absolute left-[60px] bottom-0'>
+          <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={`scale(-1,1)`} />
+        </div>
+
       </div>
 
     </div>

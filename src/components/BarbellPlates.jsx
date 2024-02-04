dedcddf23a2f58ac9d9c97d774809c01df7b0352
PeytonCurr <peyton.curr@gmail.com>
2024-02-03 18:20:34 -0700

    Have some plates conditionaly rendering in with accurate sizes

--- src/components/BarbellPlates.jsx
+++ src/components/BarbellPlates.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import BarBell from './svgs/BarBell.jsx'
 import BasePlate45_35 from './svgs/BasePlate45_35.jsx'
+import BasePlate10_5 from './svgs/BasePlate10_5.jsx'
+import BasePlate2_5 from './svgs/BasePlate2_5.jsx'
 import AddPlate45_35 from './svgs/AddPlate45_35.jsx'
 import EndBar from './svgs/EndBar.jsx'
 import SmartSVG from './SmartSVG.jsx'
 
 
 function BarbellPlates(props) {
-  const plates = [45, 35, 25, 10, 5, 2.5];
+  let name = BasePlate45_35
+
+  let isBroken = false
+  let countI = 0
 
   return (
     <div className='text-2xl p-2 bg-blue-300 row-span-2 md:row-span-3 rounded shadow flex justify-center items-center'>
@@ -16,26 +21,59 @@ function BarbellPlates(props) {
         <BasePlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={"translate(0 0)"} />
       </div>
 
-      {Object.entries(props.counts).map(([plate, value]) => (Array(value).fill(0).map(() => (
-        <>
-
-        </>
-      )))
-      )}
-
       <div className='w-1/2'><BarBell /></div>
 
       <div className='w-1/12 relative'>
 
-        <SmartSVG name={BasePlate45_35} color={" " + (props.counts[45] > 0 && '#DC2626')} transform={"scale(-1,1) translate(0 0)"} />
+        {Object.entries(props.counts).map(([plate, value]) => {
+          if (isBroken) {
+            return
+          }
+          if (value > 0) {
+            isBroken = true
 
-        <div className='w-full absolute left-[42%] bottom-0'>
-          <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={`scale(-1,1)`} />
-        </div>
+            if (plate == '45' || plate == '35' || plate == '25') {
+              name = BasePlate45_35
+            }
+            else if (plate == '10' || plate == '5') {
+              name = BasePlate10_5
+            }
+            else if (plate == '2.5') {
+              name = BasePlate2_5
+            }
+            return (
+              <>
+                <SmartSVG name={name} color={" " + (props.counts[plate] > 0 && '#DC2626')} transform={"scale(-1,1) translate(0 0)"} />
+              </>
+            )
+          }
 
-        <div className={'w-full absolute bottom-0 left-[84%]'}>
-          <AddPlate45_35 color={" " + (props.counts[45] > 0 && '#DC2626')} transform={`scale(-1,1)`} />
-        </div>
+        }
+        )}
+
+        {Object.entries(props.counts).map(([plate, value]) => {
+          countI = 0
+          return (
+            <>
+              {Array(value).fill(0 + 1, 0, value).map((v) => {
+
+                countI += 1
+
+                return (
+                  <>
+
+                    <div className={'w-full absolute bottom-0 ' + (`left-[${42 * countI}%]`)} >
+                      <AddPlate45_35 color={" " + (props.counts[plate] > 0 && '#DC2626')} transform={`scale(-1,1)`} />
+                    </div>
+
+                  </>
+                )
+              }
+              )}
+            </>
+          )
+        }
+        )}
 
       </div>
 

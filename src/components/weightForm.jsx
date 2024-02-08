43e6f00dd9981d7f27c4388c0f00b6982135389e
PeytonCurr <peyton.curr@gmail.com>
2024-02-07 18:55:23 -0700

    Adding some alerts to stop a couple bugs

--- src/components/weightForm.jsx
+++ src/components/weightForm.jsx
@@ -8,6 +8,12 @@ function WeightForm(props) {
   }
 
   function handleChange(event) {
+    if (props.single) {
+      if (event.target.value > 500) {
+        alert('500 is the maximum weight that can be displayed on one side');
+        return
+      }
+    }
     props.setWeight(event.target.value)
     if (props.weight <= 45 && props.barBell == true) {
       props.setBarBell(false)
@@ -15,15 +21,16 @@ function WeightForm(props) {
   }
 
   return (
-    <div className='items-center rounded bg-blue-300 shadow row-span-1 md:row-span-2 grid grid-cols-10 gap-3 px-4'>
-      <form onSubmit={handleSubmit} className=' bg-blue-50 rounded shadow p-6 pt-4 col-span-6 text-center'>
+    <div className='items-center rounded bg-blue-300 shadow row-span-2 grid grid-cols-10 grid-rows-2 sm:grid-rows-1 gap-3 px-4 py-3 sm:py-0'>
+
+      <form onSubmit={handleSubmit} className=' bg-blue-50 rounded shadow p-4 pb-6 sm:p-6 sm:pt-4 col-span-10 sm:col-span-6 text-center'>
         <h1 className='mb-2 text-center'>
           Enter Weight
         </h1>
         <input value={props?.weight || ""} type="number" step={5} max={(props.single ? 500 : 1000)} onChange={handleChange} className="text-center  w-full rounded p-1 px-2 border-2" />
       </form>
 
-      <form className=' bg-blue-50 rounded shadow py-6 col-span-2 text-center'>
+      <form className=' bg-blue-50 rounded shadow py-6 col-span-5 sm:col-span-2 text-center'>
         <h1 className='mb-3'>45lb Barbell?</h1>
         <input className="mb-2" type="checkbox" checked={props.barBell} onChange={e => {
           props.setBarBell(e.target.checked)
@@ -33,9 +40,15 @@ function WeightForm(props) {
         }} />
       </form>
 
-      <form className=' bg-blue-50 rounded shadow py-6 col-span-2 text-center'>
+      <form className=' bg-blue-50 rounded shadow py-6 col-span-5 sm:col-span-2 text-center'>
         <h1 className='mb-3'>Single Side?</h1>
         <input className="mb-2" type="checkbox" checked={props.single} onChange={e => {
+          if (!props.single) {
+            if (props.weight > 500) {
+              alert('500 is the maximum weight that can be displayed on one side');
+              return
+            }
+          }
           props.setSingle(e.target.checked)
           if (props.barBell == true) {
             props.setBarBell(!e.target.checked)

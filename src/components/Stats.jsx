8c794d344a360e85ea70c891e3fbff84a7fb99a5
PeytonCurr <peyton.curr@gmail.com>
2024-02-10 14:59:44 -0700

    Fixing some mobile view stuff

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -31,23 +31,23 @@ function Stats(props) {
   }
 
   return (
-    <div className={'bg-blue-300 rounded shadow grid  p-2 px-4 gap-4 row-span-1 md:row-span-2 ' + (props.single ? 'grid-cols-1' : 'grid-cols-2')}>
+    <div className={'bg-blue-300 rounded shadow grid  p-2 sm:px-4 gap-2 sm:gap-4 row-span-1 md:row-span-2 ' + (props.single ? 'grid-cols-1' : 'grid-cols-2')}>
 
       <div className=' bg-blue-50 rounded shadow p-6 py-2'>
         <ul className='flex h-full justify-center items-center'>
           {plates.map((plate) => (
             <>
               {props.counts[plate] > 0 &&
-                <li key={plate} className='p-2'>
+                <li key={plate} className='p-1 sm:p-2'>
                   <div className='text-center'>
                     <p
                       className={
-                        'border-2 rounded-full p-2 '
-                        + (plate == 45 && ' px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-2 border-gray-700 bg-gray-300 ')}>
+                        'border-2 rounded-full p-2 text-xs sm:text-base '
+                        + (plate == 45 && ' px-2 sm:px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2 sm:px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2 sm:px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-2.5 sm:px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3 sm:px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-1.5 sm:px-2 border-gray-700 bg-gray-300 ')}>
                       {plate}
                     </p>
-                    <p className=''> x </p>
-                    <p className=''>
+                    <p className='text-xs sm:text-base'> x </p>
+                    <p className='text-xs sm:text-base'>
                       {props.counts[plate]}
                     </p>
                   </div>
@@ -64,16 +64,16 @@ function Stats(props) {
             {plates.map((plate) => (
               <>
                 {props.counts[plate] > 0 &&
-                  <li key={plate} className='p-2'>
+                  <li key={plate} className='p-1 sm:p-2'>
                     <div className='text-center'>
                       <p
                         className={
-                          'border-2 rounded-full p-2 '
-                          + (plate == 45 && ' px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-2 border-gray-700 bg-gray-300 ')}>
+                          'border-2 rounded-full p-2 text-xs sm:text-base '
+                          + (plate == 45 && ' px-2 sm:px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2 sm:px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2 sm:px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-2.5 sm:px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3 sm:px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-1.5 sm:px-2 border-gray-700 bg-gray-300 ')}>
                         {plate}
                       </p>
-                      <p className=''> x </p>
-                      <p className=''>
+                      <p className='text-xs sm:text-base'> x </p>
+                      <p className='text-xs sm:text-base'>
                         {props.counts[plate]}
                       </p>
                     </div>

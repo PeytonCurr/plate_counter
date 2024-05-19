86d4923a3587fbbcb4067055e23ce05de8f59eee
PeytonCurr <peyton.curr@gmail.com>
2024-05-18 23:27:58 -0600

    Got Left and Right sides looking how I want. Now I just need to make them taller

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -37,19 +37,21 @@ function Stats(props) {
 
       <div className={' bg-slate-700 py-2 h-full' + (props.barBell ? ' col-span-2 rounded-l pr-2 md:pr-6 ' : ' shadow rounded px-2 sm:px-6 ')}>
         {(!props.single && !props.barBell) &&
-          <div className='absolute ps-1 sm:ps-0 sm:pt-1 ' >L</div>
+          <div className='absolute xs:ps-1 sm:pt-1 text-xs xs:text-base ' >L</div>
         }
         <ul className={'h-full items-center sm:flex grid grid-cols-2 grid-flow-dense ' + (props.barBell ? ' flex-row-reverse justify-start ' : ' justify-center ')}>
           {plates.filter(plate => props.counts[plate] > 0).map((plate, index) => (
-            <li key={plate} className={'p-1 sm:p-2 flex ' + (index == 0 && ' col-start-2 row-start-1 justify-center ') + (index == 1 && ' col-start-1 row-start-1 justify-end ') + (index == 2 && ' col-start-2 row-start-2 justify-center ') + (index == 3 && ' col-start-1 row-start-2 justify-end ')}>
-              <div className='text-center w-min'>
-                <p
-                  className={
-                    'text-black border-2 rounded-full p-2 text-xs sm:text-base '
-                    + (plate == 45 && ' px-2 sm:px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2 sm:px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2 sm:px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-2.5 sm:px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3 sm:px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-1.5 sm:px-2 border-gray-700 bg-gray-300 ')}>
-                  {plate}
-                </p>
-                <p className='text-xs sm:text-base'> x </p>
+            <li key={plate} className={'p-1 sm:p-2 flex ' + ((props.barBell && (index == 1 || index == 3)) && ' justify-end col-start-1 ') + ((props.barBell && (index == 0 || index == 2)) && ' justify-center col-start-2 ') + ((!props.barBell && (index == 1 || index == 3)) && ' col-start-1 justify-end pe-1.5 xs:pe-3 ') + ((!props.barBell && (index == 0 || index == 2)) && ' col-start-2 justify-start ps-1.5 xs:ps-3 ')}>
+              <div className='text-center w-fit sm:w-full flex flex-row-reverse sm:flex-col items-center justify-start'>
+                <div className='w-1/2 sm:w-auto flex justify-start'>
+                  <div
+                    className={
+                      'text-black border-2 rounded-full p-2 text-xs sm:text-base w-min '
+                      + (plate == 45 && ' px-2 sm:px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2 sm:px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2 sm:px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-2.5 sm:px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3 sm:px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-1.5 sm:px-2 border-gray-700 bg-gray-300 ')}>
+                    {plate}
+                  </div>
+                </div>
+                <p className='text-xs sm:text-base p-0.5 xxs:p-1 xs:1.5 sm:p-0'> x </p>
                 <p className='text-xs sm:text-base'>
                   {props.counts[plate]}
                 </p>
@@ -71,30 +73,28 @@ function Stats(props) {
       }
 
 
-      <div className={' bg-slate-700 py-2 h-full ' + (props.single && ' w-[103%] ') + (props.barBell ? ' col-span-2 rounded-r pl-2 xs:pl-6 ' : ' shadow rounded px-2 sm:px-6 ')}>
+      <div className={' bg-slate-700 py-2 h-full ' + (props.single && ' w-[103%] ') + (props.barBell ? ' col-span-2 rounded-r pl-2 md:pl-6 ' : ' shadow rounded px-2 sm:px-6 ')}>
         {(!props.single && !props.barBell) &&
-          <div className='absolute ps-1 sm:ps-0 sm:pt-1' >R</div>
+          <div className='absolute xs:ps-1 sm:pt-1 text-xs xs:text-base' >R</div>
         }
-        <ul className={'flex h-full items-center ' + (props.barBell ? ' justify-start ' : ' justify-center ')}>
-          {plates.map((plate) => (
-            <>
-              {props.counts[plate] > 0 &&
-                <li key={plate} className='p-1 sm:p-2'>
-                  <div className='text-center'>
-                    <p
-                      className={
-                        'text-black border-2 rounded-full p-2 text-xs sm:text-base '
-                        + (plate == 45 && ' px-2 sm:px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2 sm:px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2 sm:px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-2.5 sm:px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3 sm:px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-1.5 sm:px-2 border-gray-700 bg-gray-300 ')}>
-                      {plate}
-                    </p>
-                    <p className='text-xs sm:text-base'> x </p>
-                    <p className='text-xs sm:text-base'>
-                      {props.counts[plate]}
-                    </p>
-                  </div>
-                </li>
-              }
-            </>
+        <ul className={'h-full items-center sm:flex grid grid-cols-2 ' + (props.barBell ? ' justify-start ' : ' justify-center ')}>
+          {plates.filter(plate => props.counts[plate] > 0).map((plate, index) => (
+            <li key={plate} className={'p-1 sm:p-2 flex ' + ((props.barBell && (index == 1 || index == 3)) && ' justify-start ') + ((props.barBell && (index == 0 || index == 2)) && ' justify-center ') + ((!props.barBell && (index == 1 || index == 3)) && ' justify-start ps-1.5 xs:ps-3 ') + ((!props.barBell && (index == 0 || index == 2)) && ' justify-end pe-1.5 xs:pe-3 ')}>
+              <div className='text-center w-fit-content sm:w-full flex sm:flex-col items-center'>
+                <div className='w-1/2 sm:w-auto flex justify-end'>
+                  <p
+                    className={
+                      'text-black border-2 rounded-full p-2 text-xs sm:text-base w-min '
+                      + (plate == 45 && ' px-2 sm:px-2.5 border-red-600 bg-red-100 ') + (plate == 35 && ' px-2 sm:px-2.5 border-blue-600 bg-blue-100 ') + (plate == 25 && ' px-2 sm:px-2.5 border-yellow-600 bg-yellow-100 ') + (plate == 10 && ' px-2.5 sm:px-3 border-green-600 bg-green-100 ') + (plate == 5 && ' px-3 sm:px-3.5 border-zinc-950 bg-zinc-400 ') + (plate == 2.5 && ' px-1.5 sm:px-2 border-gray-700 bg-gray-300 ')}>
+                    {plate}
+                  </p>
+                </div>
+                <p className='text-xs sm:text-base p-0.5 xxs:p-1 xs:1.5 sm:p-0'> x </p>
+                <p className='text-xs sm:text-base'>
+                  {props.counts[plate]}
+                </p>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
